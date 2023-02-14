53fae66c0ad84293818b663d780234b3b3e2e30a
paullaster <paullasterokoth98@gmail.com>
2023-02-14 17:56:24 +0300

    commit files

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ form.addEventListener('submit', (event) =>{
         if (!input.validity.valid && !textArea.validity.valid){
             const errorSpan = input.nextElementSibling;
             showError(input, errorSpan);
-
+            //Displaying error below the comment box
             const errorSpanTextArea = input.nextElementSibling;
-            showError(textArea, errorSpanTextArea)
+            showError(textArea, errorSpanTextArea);
         }
     })
 })
