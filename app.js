09bd67d0c13a3828145b058b204aa3a11fd99f00
paullaster <paullasterokoth98@gmail.com>
2023-02-14 16:22:59 +0300

    commit files

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const form = document.querySelector("form");
 const inputs = document.querySelectorAll(
   "input"
 );
-const errorSpan = document.querySelectorAll("input + span.error");
-
-console.log(errorSpan);
 
 Array.from(inputs).forEach( (input) => {
     input.addEventListener("input", (event) => {
         if(input.validity.valid){
-            
+            const errorSpan = document.querySelectorAll("input + span.error");
         }
     })
 })
